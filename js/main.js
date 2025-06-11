03d79b7177e8beffb178a9874d8456c64472bb90
parksangseok <park0157@naver.com>
2025-06-12 01:23:54 +0900

    commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+$(function () {
+
+  //aos 초기화
+  $(window).load(function () {
+    AOS.init({
+      duration: 2000
+    });
+    $('#main .infoWrap .infoBox .img01').addClass('active');
+    $('#main .infoWrap .infoBox .img02').addClass('active');
+  });
+
+});
